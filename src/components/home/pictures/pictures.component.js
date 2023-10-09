b210dadd0ccdfafa101a823219da48ffecbd4c3e
Michael Babin <mic.babin@outlook.com>
2023-10-09 12:04:00 -0400

    fix mages array

--- src/components/home/pictures/pictures.component.js
+++ src/components/home/pictures/pictures.component.js
@@ -20,7 +20,25 @@ const Pictures = () => {
     }
   `)
 
-  const images = data.allWpMediaItem.nodes
+  const sortImagesByDescription = images => {
+    // Use the sort method to sort the images array
+    images.sort((a, b) => {
+      // Extract descriptions from the images or set them to empty strings if undefined
+      const descriptionA = a.description || ""
+      const descriptionB = b.description || ""
+
+      // Use localeCompare to perform a case-insensitive string comparison
+      return descriptionA.localeCompare(descriptionB, undefined, {
+        sensitivity: "base",
+      })
+    })
+
+    // Return the sorted array
+    return images
+  }
+
+  const imagesArr = data.allWpMediaItem.nodes
+  const images = sortImagesByDescription(imagesArr)
 
   useEffect(() => {
     const handleScroll = () => {
